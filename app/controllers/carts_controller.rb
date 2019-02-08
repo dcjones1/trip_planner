954e932653326da2594c54a8361ccb761a0acd50
dcjones1 <dcejjones@gmail.com>
2019-02-08 15:52:56 -0500

    models and controllers bare bones

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,23 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:edit, :show]
+
+  def update
+    @cart.update(cart_params)
+    if @cart.save
+      redirect_to @cart
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cart.destroy
+    redirect_to carts_path
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
 end
--- app/controllers/transits_controller.rb
+++ app/controllers/transits_controller.rb
@@ -1,2 +1,13 @@
 class TransitsController < ApplicationController
+  before_action :set_transit, only: [:edit, :destroy, :show]
+
+  def index
+    @transits = Transit.all
+  end
+
+  private
+
+  def set_transit
+    @transit = transit.find(params[:id])
+  end
 end
