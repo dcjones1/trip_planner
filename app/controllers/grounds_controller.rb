52f16f5153bd50fb015ae2e66b4a09915f7f8baa
dcjones1 <dcejjones@gmail.com>
2019-02-14 14:17:27 -0500

    finalized flow

--- app/controllers/grounds_controller.rb
+++ app/controllers/grounds_controller.rb
@@ -10,16 +10,14 @@ class GroundsController < ApplicationController
   end
 
   def create
-    session[:airport] = ground_params[:destination]
     @ground = Ground.new({origin: ground_params[:origin], destination: ground_params[:destination],mode: ground_params[:mode]})
     @airport = Api.new
     @hash = @airport.maps_call({origin: ground_params[:origin], destination: ground_params[:destination],mode: ground_params[:mode]})
     @ground.duration = @airport.get_duration(@hash)
     @ground.directions = @airport.get_directions_array(@hash)
-    #NOT CORRECT FIX ME
     @ground.trip = current_trip
     if @ground.save
-      redirect_to search_path
+      redirect_to current_trip
     else
       render :new
     end
