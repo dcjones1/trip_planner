fb20e9285a55831f3308cbf28a1fc026b5ac8284
dcjones1 <dcejjones@gmail.com>
2019-02-15 14:09:36 -0500

    removed for heroku

--- config/application.rb
+++ config/application.rb
@@ -13,15 +13,7 @@ module TripPlanner
 
     config.assets.paths << Rails.root.join('node_modules')
 
-    config_files = ['secrets.yml']
-
-    config_files.each do |file_name|
-      file_path = File.join(Rails.root, 'config', file_name)
-      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
-      config_keys.each do |k,v|
-        ENV[k.upcase] ||= v
-      end
-    end
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
